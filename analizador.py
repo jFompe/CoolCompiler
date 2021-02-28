bc1fe8194d53903556e070bec511ac5f48786870
jaimefomperosa <jaime.fomperosa@fieldeas.com>
2021-02-28 20:00:05 +0100

    task 1_3

--- analizador.py
+++ analizador.py
@@ -4,7 +4,12 @@ from sly import Lexer
 
 class SimpleLexer(Lexer):
     # Token names
-    tokens = { NUMBER, ID }
+    tokens = { 
+        NUMBER, ID, ASSIGN, 
+        PLUS, TIMES, 
+        LPAREN, RPAREN,
+        LE, GE, LT, GT, NE
+    }
 
     # Ignored characters
     ignore = ' \t'
@@ -12,6 +17,17 @@ class SimpleLexer(Lexer):
     # Token regexs
     NUMBER = r'\d+'
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ASSIGN = r'='
+    PLUS = r'\+'
+    TIMES = r'\*'
+    LPAREN = r'\('
+    RPAREN = r'\)'
+    LE = r'<='
+    GE = r'>='
+    LT = r'<'
+    GT = r'>'
+    NE = r'!='
+
 
     @_(r'\n+')
     def ignore_newline(self,  t):
@@ -23,7 +39,7 @@ class SimpleLexer(Lexer):
 
 # Example
 if __name__ == '__main__':
-    text = 'abc 123 $ cde 456'
+    text = 'a = 3 + (4 * 5)'
     lexer = SimpleLexer()
     for tok in lexer.tokenize(text):
         print(tok)
