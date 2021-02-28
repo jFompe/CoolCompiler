b7cdeb7d79af6c69f985cf702bb4280a60cf1488
jaimefomperosa <jaime.fomperosa@fieldeas.com>
2021-02-28 19:43:29 +0100

    task 1_1

--- ex1.py
+++ ex1.py
@@ -5,10 +5,12 @@ ID = r'(?P<ID>[a-zA-Z_][a-zA-Z0-9_]*)'
 NUMBER = r'(?P<NUMBER>\d+)'
 SPACE = r'(?P<SPACE>\s+)'
 
-patterns = [ID, NUMBER, SPACE]
+patterns = [ID, NUMBER]
+ignored_patterns = [SPACE]
 
 # Expresión regular general
 pat = re.compile('|'.join(patterns))
+pat_ignore = re.compile('|'.join(ignored_patterns))
 
 def tokenize(text):
     index = 0
@@ -17,8 +19,12 @@ def tokenize(text):
         if m:
             yield (m.lastgroup, m.group())
             index = m.end()
-        else:
-            raise SyntaxError('Bad char %r' % text[index])
+            continue
+        ign_m = pat_ignore.match(text,index)
+        if ign_m:
+            index = ign_m.end()
+            continue    
+        raise SyntaxError('Bad char %r' % text[index])
 
 # Ejemplo de uso
 text = 'abc 123 cde 456'
